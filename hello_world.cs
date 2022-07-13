8e3a16f3c3ef6b1f2116e869a7ebdf6a81b6093d
HIJIKI777 <kuboyoshi0318@icloud.com>
2022-07-13 10:48:11 +0900

    C# study

---
+++ hello_world.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello_world
+{
+    class HelloWorld{
+        static void Main(string[] args){
+            Console.WriteLine("HelloWorld");
+        }
+    }
+}
